bf7781c2451e1b3558f9ffeaa0a9e1f259b84821
Your Name <you@example.com>
2023-04-18 14:40:12 +0200

    eslam

---
+++ project/resources/views/student.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<ul>
+   <h2> <li>mostafa</li>
+    <li>ali</li>
+    <li>zahra</li>
+    <li>mohamed</li>
+    <li>hamdy</li></h2>
+
+</ul>
+</body>
+</html>
